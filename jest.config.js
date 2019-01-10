df15664b0699c764ad0b0a61ee3d06b8a9020eaf
Joe Banks <joeb@23andme.com>
2019-01-10 13:09:02 -0800

    Mock images in tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '^.+\\.(gif|png|jpg|ttf|eot)\\?(resize|sizes)(.+)?$':
+      '<rootDir>/__mocks__/fileResizeMock.js',
+    '^.+\\.(gif|png|jpg|ttf|eot)(\\?.+)?$': '<rootDir>/__mocks__/fileMock.js',
+    '^.+\\.svg\\?(sprite|include)(.+)?$': '<rootDir>/__mocks__/svgMock.js',
+  },
 }
